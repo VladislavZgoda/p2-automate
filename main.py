a5be6cc7aa357ad1d879a1fe9ab907a9d23447ba
zgodavg <www.vlad78@yandex.ru>
2026-01-19 11:01:39 +0300

    Second draft

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from playwright.sync_api import Playwright, sync_playwright
 
+URL = getenv("URL")
+LOGIN = getenv("LOGIN")
+PASSWORD = getenv("PASSWORD")
+
+if not URL or not LOGIN or not PASSWORD:
+    print("Error! Some env vars are not defined!")
+    exit(1)
+
+meters: list[str] = ["51424969", "50556827"]
+
 script_dir = Path(__file__).resolve().parent
 
 current_date = datetime.now()
@@ -21,11 +31,11 @@ def run(playwright: Playwright) -> None:
     context = browser.new_context(ignore_https_errors=True)
     page = context.new_page()
 
-    page.goto(getenv("URL"))
+    page.goto(URL)
     page.locator("#userName").click()
-    page.locator("#userName").fill(getenv("LOGIN"))
+    page.locator("#userName").fill(LOGIN)
     page.locator("#password").click()
-    page.locator("#password").fill(getenv("PASSWORD"))
+    page.locator("#password").fill(PASSWORD)
     page.get_by_role("button", name="EnterA").click()
 
     page.locator(".menu-group-interaction").first.click()
@@ -33,14 +43,15 @@ def run(playwright: Playwright) -> None:
     page.locator("dx-drop-down-box").get_by_role("combobox").click()
     page.locator("dx-button").nth(2).click()
     page.get_by_role("menu").get_by_text("Географические объекты").click()
-    page.get_by_title("Поиск").click()
-    page.get_by_text("Поиск по наименованию").click()
-    page.get_by_role("textbox").click()
-    page.get_by_role("textbox").press_sequentially("51424969")
-    page.locator("button").nth(4).click()
-    page.get_by_role(
-        "gridcell", name="Меркурий 230 ART-03 PQRSIDN, №51424969"
-    ).get_by_label("Выбрать строку").click()
+
+    for meter in meters:
+        page.get_by_title("Поиск").click()
+        page.get_by_text("Поиск по наименованию").click()
+        page.get_by_role("textbox").click()
+        page.get_by_role("textbox").press_sequentially(meter)
+        page.locator("button").nth(4).click()
+        page.get_by_role("gridcell", name=meter).get_by_label("Выбрать строку").click()
+        page.get_by_title("Удалить").locator("button").click()
 
     page.get_by_role("button", name="OK").click()
     page.get_by_role("checkbox", name="A+").click()
@@ -67,6 +78,7 @@ def run(playwright: Playwright) -> None:
 
     with page.expect_download() as download_info:
         page.get_by_role("button", name="Excel").click()
+
     download = download_info.value
     download.save_as(script_dir / "download" / download.suggested_filename)
 
