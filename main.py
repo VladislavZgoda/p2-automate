8e4fb49e4d5e02e917a2634ca43d7a8b40800704
zgodavg <www.vlad78@yandex.ru>
2026-01-26 15:13:56 +0300

    Refactor code

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ previous_month = str(previous_month_date.month).zfill(2)
 previous_year = str(previous_month_date.year)
 
 
-def run(playwright: Playwright, profile: Profile) -> None:
+def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False, slow_mo=1000)
     context = browser.new_context(ignore_https_errors=True)
     page = context.new_page()
@@ -56,54 +56,60 @@ def run(playwright: Playwright, profile: Profile) -> None:
 
     page.locator(".menu-group-interaction").first.click()
     page.get_by_text("Новый профиль энергии").click()
-    page.locator("dx-drop-down-box").get_by_role("combobox").click()
-    page.locator("dx-button").nth(2).click()
 
-    menu_type = "НЭСК" if profile["menu_type"] == "НЭСК" else "Географические объекты"
-    page.get_by_role("menu").get_by_text(menu_type).click()
+    for profile in profiles:
+        page.locator("dx-drop-down-box").get_by_role("combobox").click()
+        page.locator("dx-button").nth(2).click()
 
-    for meter in profile["meters"]:
-        page.get_by_title("Поиск").click()
-        page.get_by_text("Поиск по наименованию").click()
-        page.get_by_role("textbox").click()
-        page.get_by_role("textbox").press_sequentially(meter)
-        page.locator("button").nth(4).click()
-        page.get_by_role("gridcell", name=meter).get_by_label("Выбрать строку").click()
-        page.get_by_title("Удалить").locator("button").click()
+        menu_type = (
+            "НЭСК" if profile["menu_type"] == "НЭСК" else "Географические объекты"
+        )
+        page.get_by_role("menu").get_by_text(menu_type).click()
 
-    page.get_by_role("button", name="OK").click()
-    page.get_by_role("checkbox", name="A+").click()
+        for meter in profile["meters"]:
+            page.get_by_title("Поиск").click()
+            page.get_by_text("Поиск по наименованию").click()
+            page.get_by_role("textbox").click()
+            page.get_by_role("textbox").press_sequentially(meter)
+            page.locator("button").nth(4).click()
+            page.get_by_role("gridcell", name=meter).get_by_label(
+                "Выбрать строку"
+            ).click()
+            page.get_by_title("Удалить").locator("button").click()
 
-    page.get_by_role("combobox").nth(1).click()
-    page.get_by_role("combobox").nth(1).press("ArrowLeft")
-    page.get_by_role("combobox").nth(1).press("ArrowLeft")
-    page.get_by_role("combobox").nth(1).press_sequentially("01")
-    page.get_by_role("combobox").nth(1).press_sequentially(previous_month)
-    page.get_by_role("combobox").nth(1).press_sequentially(previous_year)
+        page.get_by_role("button", name="OK").click()
+        page.get_by_role("checkbox", name="A+").click()
 
-    for _ in range(3):
-        page.get_by_role("combobox").nth(1).press_sequentially("00")
+        page.get_by_role("combobox").nth(1).click()
+        page.get_by_role("combobox").nth(1).press("ArrowLeft")
+        page.get_by_role("combobox").nth(1).press("ArrowLeft")
+        page.get_by_role("combobox").nth(1).press_sequentially("01")
+        page.get_by_role("combobox").nth(1).press_sequentially(previous_month)
+        page.get_by_role("combobox").nth(1).press_sequentially(previous_year)
 
-    page.get_by_role("combobox").nth(2).click()
-    page.get_by_role("combobox").nth(2).press("ArrowLeft")
-    page.get_by_role("combobox").nth(2).press("ArrowLeft")
-    page.get_by_role("combobox").nth(2).press_sequentially("01")
-    page.get_by_role("combobox").nth(2).press_sequentially(current_month)
-    page.get_by_role("combobox").nth(2).press_sequentially(current_year)
+        for _ in range(3):
+            page.get_by_role("combobox").nth(1).press_sequentially("00")
 
-    for _ in range(3):
-        page.get_by_role("combobox").nth(2).press_sequentially("00")
+        page.get_by_role("combobox").nth(2).click()
+        page.get_by_role("combobox").nth(2).press("ArrowLeft")
+        page.get_by_role("combobox").nth(2).press("ArrowLeft")
+        page.get_by_role("combobox").nth(2).press_sequentially("01")
+        page.get_by_role("combobox").nth(2).press_sequentially(current_month)
+        page.get_by_role("combobox").nth(2).press_sequentially(current_year)
 
-    with page.expect_download() as download_info:
-        page.get_by_role("button", name="Excel").click()
+        for _ in range(3):
+            page.get_by_role("combobox").nth(2).press_sequentially("00")
 
-    download = download_info.value
-    download.save_as(script_dir / "download" / profile["file_name"])
+        with page.expect_download() as download_info:
+            page.get_by_role("button", name="Excel").click()
+
+        download = download_info.value
+        download.save_as(script_dir / "download" / profile["file_name"])
+        page.reload()
 
     context.close()
     browser.close()
 
 
-for profile in profiles:
-    with sync_playwright() as playwright:
-        run(playwright, profile)
+with sync_playwright() as playwright:
+    run(playwright)
